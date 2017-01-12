bd280ae6cf52fd8322cc65dfafa4491e0778fd47
dddisk <13da144@gmail.com>
2017-01-12 14:00:40 +0900

    edit group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,14 +3,22 @@ class GroupsController < ApplicationController
   end
   
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
+    redirect_to controller: :messages, action: :index
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => []})
   end
 end
