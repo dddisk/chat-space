e00de3e2fa50b62d7bca3c85a43e2ad6505fa7c2
dddisk <13da144@gmail.com>
2017-01-12 12:49:35 +0900

    create colum

--- db/migrate/20170112031645_create_user_groups.rb
+++ db/migrate/20170112031645_create_user_groups.rb
@@ -1,7 +1,8 @@
 class CreateUserGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :user_groups do |t|
-
+      t.references :group
+      t.references :user
       t.timestamps
     end
   end
