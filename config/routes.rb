a18b19675c0c6866acf2511f90ff60145ca10bee
dddisk <13da144@gmail.com>
2017-01-19 19:18:02 +0900

    edit root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
 
   resources :groups, only:[:new, :edit, :create] do
     resources :messages, only: [:index]
