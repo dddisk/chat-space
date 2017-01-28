cb767fa027520614703ed38eb36e6e74dc16052e
dddisk <13da144@gmail.com>
2017-01-28 15:36:37 +0900

    fix pass and controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-    redirect_to group_messages_path(@group), notice: 'チャットグループが更新されました'
+      redirect_to group_messages_path(@group), notice: 'チャットグループが更新されました'
     else
       render :edit
     end
