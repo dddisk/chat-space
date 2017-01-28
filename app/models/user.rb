7a3f4cef87e6e15e36cb0f5248d9f50a21997d5d
dddisk <13da144@gmail.com>
2017-01-28 14:43:18 +0900

    add destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :user_groups
+  has_many :user_groups, dependent: :destroy
   has_many :messages
   has_many :groups, through: :user_groups
 end
