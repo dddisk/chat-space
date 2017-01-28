90cd81ca02cdd02bcd2dd6f676763018db4cda2c
dddisk <13da144@gmail.com>
2017-01-28 14:37:25 +0900

    add update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
 
-  resources :groups, only:[:new, :edit, :create] do
+  resources :groups, only:[:new, :edit, :create, :update] do
     resources :messages, only: [:index]
   end
 end
