c6672ed8a16dbe2be8746d9794f40b3c15e261a0
dddisk <13da144@gmail.com>
2017-02-01 18:41:27 +0900

    index test add

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -5,12 +5,34 @@ describe MessagesController do
   let(:user){ create(:user) }
   let(:group){ create(:group) }
   let(:message){ create(:message) }
+  let(:groups)   { create_list(:group, 5, user_ids: user.id) }
+  let(:messages) { create_list(:message, 5, user_id: user.id, group_id: group.id) }
+
   before do
     sign_in user
   end
 
   after do
     sign_out user
+  end
+
+  describe 'GET #index' do
+    before do
+      get :index , group_id: group.id
+    end
+
+    it "assigns @group" do
+      expect(assigns(:group)).to match(group)
+    end
+
+    it "assigns @groups" do
+      expect(assigns(:groups)).to match(groups)
+    end
+
+    it "assigns @message" do
+      expect(assigns(:message)).to match(message)
+    end
+
   end
 
 
