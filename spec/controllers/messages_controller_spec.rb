95bfba7eccc67c182c90b42a0d6d2b4bce414ecb
dddisk <13da144@gmail.com>
2017-02-01 16:24:31 +0900

    edit message_controller_spec

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,1 +1,20 @@
 require 'rails_helper'
+
+describe MessagesController do
+
+  let(:user){ create(:user) }
+  let(:group){ create(:group) }
+
+  before do
+    sign_in user
+  end
+
+  after do
+    sign_out user
+  end
+
+end
+
+
+
+
